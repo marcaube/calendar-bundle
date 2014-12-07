c99602668b95bd1847774275b00d75c4acc31638
marcaube <marc@tourismecie.com>
2014-12-07 10:47:07 -0500

    Update to fullcalendar 2.0
    * update controller for new datetime format
    * fix cs
    * remove static assets in favor of CDN

--- Controller/CalendarController.php
+++ Controller/CalendarController.php
@@ -2,40 +2,39 @@
 
 namespace ADesigns\CalendarBundle\Controller;
 
+use ADesigns\CalendarBundle\Entity\EventEntity;
+use ADesigns\CalendarBundle\Event\CalendarEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
-use ADesigns\CalendarBundle\Event\CalendarEvent;
+use Symfony\Component\HttpFoundation\Response;
 
 class CalendarController extends Controller
 {
     /**
      * Dispatch a CalendarEvent and return a JSON Response of any events returned.
-     * 
-     * @param Request $request
+     *
+     * @param  Request  $request
      * @return Response
      */
     public function loadCalendarAction(Request $request)
     {
-        $startDatetime = new \DateTime();
-        $startDatetime->setTimestamp($request->get('start'));
-        
-        $endDatetime = new \DateTime();
-        $endDatetime->setTimestamp($request->get('end'));
-        
+        $startDatetime = \DateTime::createFromFormat('Y-m-d', $request->get('start'));
+        $endDatetime   = \DateTime::createFromFormat('Y-m-d', $request->get('end'));
+
         $events = $this->container->get('event_dispatcher')->dispatch(CalendarEvent::CONFIGURE, new CalendarEvent($startDatetime, $endDatetime, $request))->getEvents();
-        
-        $response = new \Symfony\Component\HttpFoundation\Response();
+
+        $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
-        
+
         $return_events = array();
-        
-        foreach($events as $event) {
-            $return_events[] = $event->toArray();    
+
+        /** @var EventEntity $event */
+        foreach ($events as $event) {
+            $return_events[] = $event->toArray();
         }
-        
+
         $response->setContent(json_encode($return_events));
-        
+
         return $response;
     }
 }
--- Event/CalendarEvent.php
+++ Event/CalendarEvent.php
@@ -2,14 +2,14 @@
 
 namespace ADesigns\CalendarBundle\Event;
 
+use ADesigns\CalendarBundle\Entity\EventEntity;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\HttpFoundation\Request;
 
-use ADesigns\CalendarBundle\Entity\EventEntity;
-
 /**
  * Event used to store EventEntitys
- * 
+ *
  * @author Mike Yudin <mikeyudin@gmail.com>
  */
 class CalendarEvent extends Event
@@ -17,36 +17,37 @@ class CalendarEvent extends Event
     const CONFIGURE = 'calendar.load_events';
 
     private $startDatetime;
-    
+
     private $endDatetime;
-    
+
     private $request;
 
     private $events;
-    
+
     /**
      * Constructor method requires a start and end time for event listeners to use.
-     * 
-     * @param \DateTime $start Begin datetime to use
-     * @param \DateTime $end End datetime to use
+     *
+     * @param \DateTime $start   Begin datetime to use
+     * @param \DateTime $end     End datetime to use
+     * @param Request   $request
      */
     public function __construct(\DateTime $start, \DateTime $end, Request $request = null)
     {
         $this->startDatetime = $start;
         $this->endDatetime = $end;
         $this->request = $request;
-        $this->events = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->events = new ArrayCollection();
     }
 
     public function getEvents()
     {
         return $this->events;
     }
-    
+
     /**
      * If the event isn't already in the list, add it
-     * 
-     * @param EventEntity $event
+     *
+     * @param  EventEntity   $event
      * @return CalendarEvent $this
      */
     public function addEvent(EventEntity $event)
@@ -54,13 +55,13 @@ class CalendarEvent extends Event
         if (!$this->events->contains($event)) {
             $this->events[] = $event;
         }
-        
+
         return $this;
     }
-    
+
     /**
-     * Get start datetime 
-     * 
+     * Get start datetime
+     *
      * @return \DateTime
      */
     public function getStartDatetime()
@@ -69,8 +70,8 @@ class CalendarEvent extends Event
     }
 
     /**
-     * Get end datetime 
-     * 
+     * Get end datetime
+     *
      * @return \DateTime
      */
     public function getEndDatetime()
@@ -80,7 +81,7 @@ class CalendarEvent extends Event
 
     /**
      * Get request
-     * 
+     *
      * @return Request
      */
     public function getRequest()
